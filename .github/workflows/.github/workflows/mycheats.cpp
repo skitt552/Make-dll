d5c823ddd33e762dac0cbfed240a50a89b326479
skitt552 <tylercassdy0211@gmail.com>
2025-08-31 02:51:24 -0400

    mycheats.cpp

---
+++ .github/workflows/.github/workflows/mycheats.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+
+// A simple DLL test
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        MessageBoxA(NULL, "DLL loaded successfully!", "Test", MB_OK);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
